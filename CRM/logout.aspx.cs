570bb1a02400bea740b80e8da41194a6241667f1
Atharv Mankar <137928350+AtharvM66@users.noreply.github.com>
2024-08-02 23:51:31 +0530

    Update logout.aspx.cs

--- CRM/logout.aspx.cs
+++ CRM/logout.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -21,11 +21,6 @@ namespace CRM
 
             // Redirect to login page
             Response.Redirect("login.aspx");
-        }
-
-        protected void Buttongoback_Click(object sender, EventArgs e)
-        {
-            Response.Redirect("home.aspx");
         }
     }
 }
